6bbcf0d49ee9fad46bae8dd643b2cfe0545ff952
Vignesh Tongaria <75326364+VigneshTongaria@users.noreply.github.com>
2022-08-14 23:40:25 +0530

    Add files via upload

---
+++ Scripts/Block Breaker/BrekableBlocks.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrekableBlocks : MonoBehaviour
+{
+    [SerializeField] Sprite CrackedBlock;
+    [SerializeField] GameObject particle;
+    [SerializeField] GameObject laser;
+    [SerializeField] int hit = 3;
+    [SerializeField] int xp;
+    [SerializeField] AudioClip blocksound;
+    int hits = 0;
+    LevelDescription level;
+    void Start()
+    {
+        gameObject.GetComponent<Transform>().localScale = new Vector3(0.3f, 0.22f,0.3f);
+        level = FindObjectOfType<LevelDescription>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        AudioSource.PlayClipAtPoint(blocksound, transform.position);
+        hits++;
+        if (hits == hit)
+        {
+           if(laser!=null) Instantiate(laser,transform.position,Quaternion.identity);
+            level.returnspecialmeter(xp);
+            Destroy(gameObject);
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().sprite = CrackedBlock;
+            GameObject part = Instantiate(particle, collision.transform.position, Quaternion.Euler(90,0,0));
+            Destroy(part, 1f);
+        }
+    }
+}
